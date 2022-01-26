f58890045c834ff1dd0bb3ac2c7bde67173ac5c9
Samit Madhesiya <97397076+samit08@users.noreply.github.com>
2022-01-26 17:17:41 +0530

    Add files via upload

---
+++ assingnment02output.py
@@ -0,0 +1,60 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======= RESTART: C:\Users\samit\Desktop\python assingment\assinment 02.py ======
+question1
+(a)
+Python is a case sensitive language
+35
+(b)
+egaugnal evitisnes esac a si nohtyP
+(c)
+a case sensitive
+(d)
+Python is object oriented language
+(e)
+10
+(f)
+Pythonisacasesensitivelanguage
+ 
+ 
+question2
+Hey,Samit Here!
+My SID is 21104008
+I am from Electrical department and my CGPA is 9.8
+ 
+ 
+question3
+(a)
+0b1000
+(b)
+0b111010
+(c)
+0b110010
+(d)
+0b11100000
+0b101000
+(e)
+0b1110
+0b0
+ 
+ 
+question4
+Give first number:10
+Give second number:12
+Give third number:13
+13 is greatest number
+ 
+ 
+question5
+Give a word:my name is samit
+Yes
+ 
+ 
+question6
+Give a number:2
+Give a number:3
+Give a number:5
+The triangle is not possible
+ 
+ 
