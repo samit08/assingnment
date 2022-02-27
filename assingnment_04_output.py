0ad6de464574ce3d37dfced4d39f89fb6c296734
Samit Madhesiya <97397076+samit08@users.noreply.github.com>
2022-02-27 18:22:50 +0530

    Add files via upload

---
+++ assingnment_04_output.py
@@ -0,0 +1,123 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======= RESTART: C:\Users\samit\Desktop\python assingment\Assignment_4.py ======
+Question.1
+Enter number of disk in tower of Hanoi:5
+
+The Disks are numbered starting from top of the tower.
+Steps to move all disks from Source Tower to Destination Tower is given below:
+
+Move disk 1 from Source Tower to Destination
+Move disk 2 from Source Tower to Auxiliary
+Move disk 1 from Destination to Auxiliary
+Move disk 3 from Source Tower to Destination
+Move disk 1 from Auxiliary to Source Tower
+Move disk 2 from Auxiliary to Destination
+Move disk 1 from Source Tower to Destination
+Move disk 4 from Source Tower to Auxiliary
+Move disk 1 from Destination to Auxiliary
+Move disk 2 from Destination to Source Tower
+Move disk 1 from Auxiliary to Source Tower
+Move disk 3 from Destination to Auxiliary
+Move disk 1 from Source Tower to Destination
+Move disk 2 from Source Tower to Auxiliary
+Move disk 1 from Destination to Auxiliary
+Move disk 5 from Source Tower to Destination
+Move disk 1 from Auxiliary to Source Tower
+Move disk 2 from Auxiliary to Destination
+Move disk 1 from Source Tower to Destination
+Move disk 3 from Auxiliary to Source Tower
+Move disk 1 from Destination to Auxiliary
+Move disk 2 from Destination to Source Tower
+Move disk 1 from Auxiliary to Source Tower
+Move disk 4 from Auxiliary to Destination
+Move disk 1 from Source Tower to Destination
+Move disk 2 from Source Tower to Auxiliary
+Move disk 1 from Destination to Auxiliary
+Move disk 3 from Source Tower to Destination
+Move disk 1 from Auxiliary to Source Tower
+Move disk 2 from Auxiliary to Destination
+Move disk 1 from Source Tower to Destination
+Question.2
+
+[Question.2](Recursive)
+The Pascle's Triangle using recursive approach is:
+
+Enter number of rows in Pascle's Triangle:8
+                                                 1                                                  
+                                                1  1                                                
+                                              1  2  1                                               
+                                             1  3  3  1                                             
+                                           1  4  6  4  1                                            
+                                         1  5  10  10  5  1                                         
+                                       1  6  15  20  15  6  1                                       
+                                     1  7  21  35  35  21  7  1                                     
+
+[Question.2](Iterative)
+The Pascle's Triangle using iterative approach is:
+
+Enter number of rows:10
+                                                 1                                                  
+                                                1  1                                                
+                                              1  2  1                                               
+                                             1  3  3  1                                             
+                                           1  4  6  4  1                                            
+                                         1  5  10  10  5  1                                         
+                                       1  6  15  20  15  6  1                                       
+                                     1  7  21  35  35  21  7  1                                     
+                                   1  8  28  56  70  56  28  8  1                                   
+                                1  9  36  84  126  126  84  36  9  1                                
+Question.3
+
+Enter an Integer:10
+Enter another Integer:15
+
+The quotient when 10 is divided by 15 is 0.
+The remainder when 10 is divided by 15 is 10.
+
+Que3.a
+Function is Not-callable
+
+Que3.b
+All result values are NOT 'non-zero'
+
+Que3.c
+Appended (4,5,6) in the values (0,10)
+Values greater than 4 is 10,5,6
+
+Que3.d
+Above result in set form is shown below:
+{10, 5, 6}
+
+Que3.e
+The above set has been converted to immutable.
+
+Que3.f
+Max value from set is 10
+Hash value of 10 is 10
+Question.4
+Hello, My name is samit and my roll no. is 21104008
+Destructor Called
+Question.5
+Employee Name is Mehak and Salary is 40000 
+Employee Name is Ashok and Salary is 50000 
+Employee Name is Viren and Salary is 60000 
+
+Que.5a
+Mehak salary Updated to 70000
+Employee Name is Mehak and Salary is 70000 
+
+Que.5b
+Employee Viren's data has been removed.
+Question.6
+
+          WELCOME TO THE FRIENDSHIP GAME
+
+Enter Player1 name:samit
+Enter Player2 name:harsh
+
+Enter Word by samit:hello
+Enter Word by harsh:olleh
+
+Friendship of samit and harsh is REAL
